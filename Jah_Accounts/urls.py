5b8060589ccdc63446158ade90973affac68f2da
pbmulinzi <pbmulinzi@gmail.com>
2024-11-28 01:34:54 +0300

    creating models

--- Jah_Accounts/urls.py
+++ Jah_Accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(),
+    path('', views.dashboard),
+    path('products/', views.products),
+    path('customers/', views.customers),
 ]
+
--- Jah_Accounts/views.py
+++ Jah_Accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'Jah_Accounts/Dashboard.html')
+
+def products(request):
+    return render(request, 'Jah_Accounts/Products.html')
+
+def customers(request):
+    return render(request, 'Jah_Accounts/Customer.html')
