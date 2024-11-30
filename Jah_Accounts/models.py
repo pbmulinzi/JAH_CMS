9dec8f0fa20525b01c641d01ec3291ca94b53912
pbmulinzi <pbmulinzi@gmail.com>
2024-11-30 22:54:10 +0300

    ..

--- Jah_Accounts/models.py
+++ Jah_Accounts/models.py
@@ -4,24 +4,27 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
     
 class Product(models.Model):
     CATEGORY = (
-        ('Floor', 'Floor'),
+        ('Flour', 'Flour'),
         ('Honey', 'Honey'),
         ('Spice', 'Spice'),
     )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null = True)
+    description = models.CharField(max_length=200, null = True, blank = True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -40,6 +43,7 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     tags = models.ManyToManyField(Tag)
+    Quantity_kgs = models.CharField(null=True, max_length=4)
 
 
     
